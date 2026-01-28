0f34843966b9429c346d65ea28bbbf7841728374
Agostina <agoslaffitte17@gmail.com>
2026-01-28 00:45:10 -0300

    se separo carpetas

--- backend/src/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-     username: { type: String, required: true, unique: true }, 
-     password: { type: String, required: true }, //se guarda encriptada
-    role: { type: String, default: 'user' } });
-
-module.exports = mongoose.model('User', userSchema);
